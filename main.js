4682475c8888b2c0e02050ec3b9796b8bd277d3b
Monica <monicamillerg@gmail.com>
2015-10-27 23:46:02 -0700

    form work

--- main.js
+++ main.js
@@ -1,28 +1,16 @@
 $(document).ready(function(){
 
+    var webAppSite = "http://json-data.herokuapp.com/forms";
 
-var webAppSite = "http://json-data.herokuapp.com/forms";
+    $.ajax({
+          url: webAppSite,
+          method: 'get',
+          dataType: 'json',
+        }).done(function(data){
 
-$.ajax({
-	  url: webAppSite,
-	  method: 'get',
-	  dataType: 'json',
-	}).done(function(data){
-
-	
-	
-	var test= {data:data};
-
-	
-
-	
-	var templateString= $("#form").text();
-	var listHTML=Mustache.render(templateString, test);
-	$("#container").append(listHTML);
-
-});
-
- 
-
-
+            var view = {data: data};
+            var templateString = $("#form").text();
+            var listHTML = Mustache.render(templateString, view);
+            $("#container").append(listHTML);
+    });
 });
